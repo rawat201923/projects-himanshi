21c1ff94790d23a0442f66903cfe917f249388f0
rawat201923 <140051821+rawat201923@users.noreply.github.com>
2024-01-31 14:59:46 +0530

    Add files via upload

---
+++ Language/language.js
@@ -0,0 +1,38 @@
+const languages = [
+    {
+      no: "0",
+      name: "Auto",
+      native: "Detect",
+      code: "auto",
+    },
+    {
+      no: "1",
+      name: "English",
+      native: "English",
+      code: "en",
+    },
+    {
+      no: "2",
+      name: "German",
+      native: "Deutsch",
+      code: "de",
+    },
+    {
+      no: "3",
+      name: "Greek",
+      native: "Ελληνικά",
+      code: "el",
+    },
+    {
+      no: "4",
+      name: "Hindi",
+      native: "हिन्दी",
+      code: "hi",
+    },
+    {
+      no: "5",
+      name: "Urdu",
+      native: "اردو",
+      code: "ur",
+    },
+  ];
