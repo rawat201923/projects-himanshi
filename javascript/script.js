c4a585769cd819d71d762b1330938207e8fb447a
rawat201923 <140051821+rawat201923@users.noreply.github.com>
2024-02-03 07:51:55 +0000

    calculator complete

--- javascript/script.js
+++ javascript/script.js
@@ -29,3 +29,31 @@ buttons.forEach(function(button){
         }
     })
 })
+
+
+// BMI CALCULATOR
+const form=document.querySelector('form');
+
+//this usecase will give you empty
+// const height= parseInt(document.querySelector('#height').value);
+
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const results = document.querySelector('#results');
+
+    // condition for all inputs not empty, value not less than 0 & only accept a number.
+    if (height === '' || height < 0 || isNaN(height)) {
+        results.innerHTML = `PLEASE GIVE A VALID VALUE ${height}`;
+    } else if (weight === '' || weight < 0 || isNaN(weight)) {
+        results.innerHTML = `PLEASE GIVE A VALID VALUE ${weight}`;
+    } else {
+        const bmi = (height / ((height * height) / 10000)).toFixed(2);
+        // show results;
+        results.innerHTML = `<span>${bmi}</span>`;
+        console.log(bmi);
+    }
+});
